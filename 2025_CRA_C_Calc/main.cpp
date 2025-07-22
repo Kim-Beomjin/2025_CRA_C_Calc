41663f8de0682e4c38cfffd7de9029cfdae3c078
yulgon <younggon5548@gmail.com>
2025-07-22 15:06:33 +0900

    yulgon_1and6

--- 2025_CRA_C_Calc/main.cpp
+++ 2025_CRA_C_Calc/main.cpp
@@ -2,13 +2,38 @@
 
 class Cal {
 public:
+	int getSum(int a, int b)
+	{
+		return a + b;
+	}
 
+	int getSumSum(int a, int b, int c)
+	{
+		return a + b + c;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
 }
 
+TEST(t1, getSum) {
+	Cal cal;
+	int a = 1, b = 2;
+	int result;
+
+	result = cal.getSum(a, b);
+	EXPECT_EQ(a + b, result);
+}
+
+TEST(t1, getSumSum) {
+	Cal cal;
+	int a = 1, b = 2, c = 3;
+	int result;
+
+	result = cal.getSumSum(a, b, c);
+	EXPECT_EQ(a + b + c, result);
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
