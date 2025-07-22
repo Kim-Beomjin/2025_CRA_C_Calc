bd6bd8435d6cda5ca578dcb22df302b35e86abda
beom-jin.kim <beom-jin.kim@samsung.com>
2025-07-22 15:19:27 +0900

    [beom-jin.kim] Add Gob UT

--- 2025_CRA_C_Calc/main.cpp
+++ 2025_CRA_C_Calc/main.cpp
@@ -17,6 +17,30 @@ TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
 }
 
+TEST(t1, GobLeftZero) {
+	Cal cal;
+	int expect = 0;
+	int real = cal.getGob(0, 1);
+
+	EXPECT_EQ(expect, real);
+}
+
+TEST(t1, GobRightZero) {
+	Cal cal;
+	int expect = 0;
+	int real = cal.getGob(1, 0);
+
+	EXPECT_EQ(expect, real);
+}
+
+TEST(t1, GobNormal) {
+	Cal cal;
+	int expect = 10;
+	int real = cal.getGob(2, 5);
+
+	EXPECT_EQ(expect, real);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
