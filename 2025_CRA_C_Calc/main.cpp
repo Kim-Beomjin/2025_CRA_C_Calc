52536d5a408a4c97e971e6ac71f37234d7a668df
jysdp21.jung <jjiyun710@gmail.com>
2025-07-22 15:18:09 +0900

    add minus testcase

--- 2025_CRA_C_Calc/main.cpp
+++ 2025_CRA_C_Calc/main.cpp
@@ -1,20 +1,29 @@
 #include "gmock/gmock.h"
-
+#include <random>
 class Cal {
 public:
 	int getGob(int a, int b) {
 		return a * b;
-  }
+	}
 
 	int getZegop(int a) { return a * a; }
 
-  int GetMinus(int a, int b) {
+	int GetMinus(int a, int b) {
 		return a - b;
 	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+TEST(calculate_test, minus)
+{
+	Cal calculator;
+	for (int i = 0; i < 1000; i++) {
+		int a = rand();
+		int b = rand();
+		EXPECT_EQ(calculator.GetMinus(a, b), (a - b));
+	}
 }
 
 int main() {
