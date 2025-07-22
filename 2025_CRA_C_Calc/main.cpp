ca5101b111c96b327776d26bd40a4a28a0ab2804
yunzhen-kim <kyj886811@gmail.com>
2025-07-22 15:17:07 +0900

    add Unit Tests
    Signed-off-by: yunzhen-kim <kyj886811@gmail.com>

--- 2025_CRA_C_Calc/main.cpp
+++ 2025_CRA_C_Calc/main.cpp
@@ -6,7 +6,7 @@ public:
 		return a * b;
   }
 
-	int getZegop(int a) { return a * a; }
+  int getZegop(int a) { return a * a; }
 
   int GetMinus(int a, int b) {
 		return a - b;
@@ -15,6 +15,21 @@ public:
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, getGobTest) {
+	Cal cal;
+	EXPECT_EQ(20, cal.getGob(4, 5));
+}
+
+TEST(t1, getZegopTest) {
+	Cal cal;
+	EXPECT_EQ(100, cal.getZegop(10));
+}
+
+TEST(t1, getMinusTest) {
+	Cal cal;
+	EXPECT_EQ(10, cal.GetMinus(20, 10));
 }
 
 int main() {
